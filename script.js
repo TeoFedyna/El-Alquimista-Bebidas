6c83f59480c0a9856d7fe276187fc853126c1957
unknown <TeoFedyna@hotmailcom>
2022-07-14 19:42:38 -0300

    Se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+const productos = [
+{bebida1 : "fernet", precio: 1000},
+{bebida2 : "vodka smirnoft", precio: 1250},
+{bebida3 : "vodka absolut", precio: 3500},
+{bebida4 : "vodka skyy", precio: 980},
+{bebida5 : "vodka grey goose", precio: 9400},
+{bebida6 : "ron havana club", precio: 1900},
+{bebida7 : "ron havana club 3 años blanco", precio: 2100},
+{bebida8 : "ron havana club 7 años", precio: 4650},
+{bebida9 : "ron malibu", precio: 2100},
+{bebida10 : "gin bombay", precio: 4100},
+{bebida11 : "gin bombay bramble raspberry blackberry", precio: 5700},
+{bebida12 : "gin beefeater", precio: 3650},
+{bebida13 : "gin tanqueray", precio: 3100},
+{bebida14 : "whisky jack daniels", precio: 6890},
+{bebida15 : "whisky grants triple wood", precio: 6100},
+{bebida16 : "whisky johnnie walker black label", precio: 5100},
+{bebida17 : "whisky chivas regal 12 años", precio: 5700}
+]
+
+let carrito = []
+
+console.log(productos);
+
+/*
+let seleccionar
+do 
+   {
+    seleccionar = prompt("Responda si o no ¿Eres mayo de edad?")
+    }
+*/
