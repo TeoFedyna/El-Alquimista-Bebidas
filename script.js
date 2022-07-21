edc908aa5850742fa321a6e7096d042ef2600b1a
TeoFedyna <TeoFedyna@hotmail.com>
2022-07-21 19:42:22 -0300

    clase producto

--- script.js
+++ script.js
@@ -17,27 +17,45 @@ do {
 } while (edad < 18 || edad > 99 || isNaN(edad));
 
 // Lista de productos
-const bebida1 = {nombre:"fernet", precrio: 1000}
-const bebida2 = {nombre:"vodka smirnoft", precrio: 1250}
-const bebida3 = {nombre:"vodka absolut", precrio: 3500}
-const bebida4 = {nombre:"vodka skyy", precrio: 980}
-const bebida5 = {nombre:"vodka grey goose", precrio: 9400}
-const bebida6 = {nombre:"ron havana club", precrio: 1900}
-const bebida7 = {nombre:"ron havana club 3 años blanco", precrio: 2100}
-const bebida8 = {nombre:"ron havana club 7 años", precrio: 4650}
-const bebida9 = {nombre:"ron malibu", precrio: 2100}
-const bebida10 = {nombre:"gin bombay", precrio: 4100}
-const bebida11 = {nombre:"gin bombay bramble raspberry blackberry", precrio: 5700}
-const bebida12 = {nombre:"gin beefeater", precrio: 3650}
-const bebida13 = {nombre:"gin tanqueray", precrio: 3100}
-const bebida14 = {nombre:"whisky jack daniels", precrio: 6890}
-const bebida15 = {nombre:"whisky grants triple wood", precrio: 6100}
-const bebida16 = {nombre:"whisky johnnie walker black label", precrio: 5100}
-const bebida17 = {nombre:"whisky chivas regal 12 años", precrio: 5700}
-    
+const bebida1 = new Producto ("fernet", "branca", 1000)
+const bebida18 = new Producto ("fernet", "branca menta", 1000)
+const bebida2 = new Producto ("vodka", "smirnoft", 1250)
+const bebida3 = new Producto ("vodka", "absolut", 3500)
+const bebida4 = new Producto ("vodka", "skyy", 980)
+const bebida5 = new Producto ("vodka", "grey goose", 9400)
+const bebida6 = new Producto ("ron", "havana club", 1900)
+const bebida7 = new Producto ("ron", "havana club 3 años blanco", 2100)
+const bebida8 = new Producto ("ron", "havana club 7 años", 4650)
+const bebida9 = new Producto ("ron", "malibu", 2100)
+const bebida10 = new Producto ("gin", "bombay", 4100)
+const bebida11 = new Producto ("gin", "bombay bramble raspberry blackberry", 5700)
+const bebida12 = new Producto ("gin", "beefeater", 3650)
+const bebida13 = new Producto ("gin", "tanqueray", 3100)
+const bebida14 = new Producto ("whisky", "jack daniels", 6890)
+const bebida15 = new Producto ("whisky", "grants triple wood", 6100)
+const bebida16 = new Producto ("whisky", "johnnie walker black label", 5100)
+const bebida17 = new Producto ("whisky", "chivas regal 12 años", 5700)
+
 let total = 0 
 let carrito = []
 
-function sumarProductos(producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8, producto9, producto10, producto11, producto12, producto13, producto14, producto15, producto16, producto17) {
-    
+class Producto {
+    constructor(tipo = "", nombre = "", precio = 900) {
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    aumentarPrecio(porcentajeAumentado) {
+        this.precio *= porcentajeAumentado
+    }
+
+    aplicarDescuento(porcentajeDescuento) {
+        this.precio -= (this.precio * porcentajeDescuento)
+    }
+
+
 }
+
+
+
